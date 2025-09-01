67abbd00d2a32cb61f30d35ce987ae6b4d2858e6
Robot-PC 04 (HS-Fulda) <robot-pc04@hs-fulda.de>
2025-09-01 18:55:10 +0200

    todays log

--- explore.sh
+++ explore.sh
@@ -1,1 +1,7 @@
-ros2 launch tutorial_pkg explore.launch.py 2>&1 | grep -v "Message Filter dropping message:"
+set -e
+colcon build --symlink-install --cmake-clean-cache
+source install/setup.bash
+mkdir -p robot_logs
+ros2 launch tutorial_pkg explore.launch.py 2>&1 \
+  | grep -v "Message Filter dropping message" \
+  | tee "robot_logs/explore_$(date +%Y-%m-%d_%H-%M-%S).log"
